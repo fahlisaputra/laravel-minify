13e0bd569dd1728137d61c46d13159cca57243bb
Muhammad Fahli Saputra <saputra@fahli.net>
2025-02-24 00:32:03 +0800

    feat: minify helper dynamically change route path

--- src/Helpers.php
+++ src/Helpers.php
@@ -1,7 +1,18 @@
 <?php
 
-function minify($file)
+/**
+ * Minify the given file path.
+ *
+ * @param string $file The file path to minify.
+ * @return string The minified url.
+ * @throws Exception
+ */
+function minify(string $file) : string
 {
+    if (config('minify.assets_enabled') === false) {
+        throw new \Exception('Minify assets is disabled');
+    }
+
     $path = resource_path($file);
     if (!file_exists($path)) {
         throw new \Exception('File not found');
@@ -10,7 +21,7 @@ function minify($file)
     // remove slash or backslash from the beginning of the file path
     $file = ltrim($file, '/\\');
 
-    $path = '_minify/'.$file;
+    $path = route('minify.assets_route', ['file' => $file]);
 
     return $path;
 }
