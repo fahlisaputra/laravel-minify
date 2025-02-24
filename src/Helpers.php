77e738bc69f608e6547b2d744da7aa5b6e6b69ab
Muhammad Fahli Saputra <saputra@fahli.net>
2025-02-24 17:08:34 +0800

    feat: implement cache check on the helper

--- src/Helpers.php
+++ src/Helpers.php
@@ -1,16 +1,47 @@
 <?php
 
-function minify($file)
+/**
+ * Minify the given file path.
+ *
+ * @param string $file The file path to minify.
+ *
+ * @throws Exception
+ *
+ * @return string The minified url.
+ */
+function minify(string $file): string
 {
-    $path = resource_path($file);
-    if (!file_exists($path)) {
-        throw new \Exception('File not found');
+    if (!config('minify.assets_enabled')) {
+        throw new \Exception('Minify assets is disabled');
     }
 
-    // remove slash or backslash from the beginning of the file path
+    $storage = config('minify.assets_storage', 'resources');
+    $cacheFile = storage_path('/framework/cache/minify.php');
+
+    if (!file_exists($cacheFile)) {
+        file_put_contents($cacheFile, "<?php\nreturn " . var_export([], true) . ";\n");
+    }
+
+    // Normalize file path
     $file = ltrim($file, '/\\');
 
-    $path = '_minify/'.$file;
+    $cache = require $cacheFile;
+    $cachedFile = $cache[$file] ?? null;
 
-    return $path;
+    $realFilePath = base_path(rtrim($storage, '/') . '/' . $file);
+    if (!file_exists($realFilePath)) {
+        throw new \Exception("Cannot create minified route. File {$realFilePath} not found");
+    }
+
+    if ($cachedFile && file_exists($cachedFile)) {
+        if (filemtime($realFilePath) > filemtime($cachedFile)) {
+            $cachedFile = null;
+        }
+    }
+
+    if ($cachedFile) {
+        return asset(str_replace(public_path(), '', $cachedFile));
+    }
+
+    return route('minify.assets', ['file' => $file]);
 }
